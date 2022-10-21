3025966635e127cec9ade5d6caa8b1a0caf8dbf7
gulmeenajunaid <gulmeenajunaid16@gmail.com>
2022-10-21 13:01:07 +0500

    script.js updated

--- script.js
+++ script.js
@@ -1,39 +1,134 @@
-let result = 0;
-console.log("hello world");
-
 const buttonList = document.querySelectorAll("button");
-const prevExpression = document.querySelector(".previous");
+const numberBtn = document.querySelectorAll(".number");
+const operationBtn = document.querySelectorAll(".operator");
+const inputExpression = document.querySelector(".input-expression");
+const computedOutput = document.querySelector(".computed-output");
+const decimalBtn = document.querySelector(".decimal");
+const backBtn = document.querySelector(".backspace-btn");
+const clearBtn = document.querySelector(".clear-btn");
+let num1 = "";
+let num2 = "";
+let operator = "";
+let result = 0;
 
 buttonList.forEach(function (button) {
-	button.addEventListener("click", (event) => {
-		console.log(event.target.textContent);
-		prevExpression.textContent =
-			prevExpression.textContent + event.target.textContent;
+	button.addEventListener("click", (e) => {
+		if (e.target.textContent !== "=") {
+			inputExpression.textContent += e.target.textContent;
+		}
 	});
 });
 
-function add(num1, num2) {
-	result = num1 + num2;
-	console.log(result);
+numberBtn.forEach((num) => {
+	num.addEventListener("click", (e) => {
+		if (operator === " ") {
+			num1 += e.target.textContent;
+			console.log("num1", num1);
+		} else {
+			num2 += e.target.textContent;
+			console.log("num2", num2);
+		}
+	});
+});
+
+operationBtn.forEach((op) => {
+	op.addEventListener("click", (e) => {
+		if (e.target.textContent !== "=") {
+			operator = e.target.textContent;
+			console.log("operator", operator);
+		} else {
+			switch (operator) {
+				case "+":
+					addition(num1, num2);
+					console.log("result:", result);
+					computedOutput.textContent = result;
+					num1 = result;
+					num2 = "";
+					break;
+
+				case "-":
+					subtraction(num1, num2);
+					console.log("result:", result);
+					computedOutput.textContent = result;
+					num1 = inputExpression.textContent = result;
+					num2 = "";
+					break;
+
+				case "x":
+					multiplication(num1, num2);
+					console.log("result:", result);
+					computedOutput.textContent = result;
+					num1 = inputExpression.textContent = result;
+					num2 = "";
+					break;
+
+				case "%":
+					percentage(num1, num2);
+					console.log("result:", result);
+					computedOutput.textContent = result;
+					num1 = inputExpression.textContent = result;
+					num2 = "";
+					break;
+
+				case "÷":
+					division(num1, num2);
+					console.log("result:", result);
+					computedOutput.textContent = result;
+					if (result === "ERROR!") {
+						num2 = num1 = inputExpression.textContent = "";
+					} else {
+						num1 = inputExpression.textContent = result;
+						num2 = "";
+					}
+					break;
+			}
+		}
+	});
+});
+
+clearBtn.addEventListener("click", () => {
+	clear();
+});
+
+backBtn.addEventListener(
+	"click",
+	() => (inputExpression.textContent = inputExpression.textContent.slice(0, -2))
+);
+
+function clear() {
+	inputExpression.textContent = " ";
+	computedOutput.textContent = " ";
+	num1 = "";
+	num2 = " ";
+	operator = " ";
+}
+
+function addition(num1, num2) {
+	result = parseFloat(num1) + parseFloat(num2);
 	return result;
 }
-function subtract(num1, num2) {
-	result = num1 - num2;
-	console.log(result);
+
+function subtraction(num1, num2) {
+	result = parseFloat(num1) - parseFloat(num2);
 	return result;
 }
-function multiply(num1, num2) {
-	result = num1 * num2;
-	console.log(result);
+
+function multiplication(num1, num2) {
+	result = parseFloat(num1) * parseFloat(num2);
 	return result;
 }
-function divide(num1, num2) {
-	result = num1 / num2;
-	console.log(result);
-	return result;
+
+function division(num1, num2) {
+	if (num2 === "0") {
+		result = "ERROR!";
+		return result;
+	} else {
+		result = parseFloat(num1) / parseFloat(num2);
+		return result;
+	}
 }
-function clearInput() {
-	result = " ";
-	console.log(result);
+
+function percentage(num1, num2) {
+	result = (parseInt(num1) / 100) * parseInt(num2);
 	return result;
 }
